6459aa6c0c93ddf66b158a2fc98f654eccac87cc
jieyu.chen <jieyu.chen@17zuoye.com>
2019-10-16 21:03:30 +0800

    add img

--- test_1/lib/view/mainTab.dart
+++ test_1/lib/view/mainTab.dart
@@ -3,9 +3,9 @@ import "package:flutter/material.dart";
 const TextStyle optionStyle = TextStyle(fontSize: 8, fontWeight: FontWeight.bold);
 
 const List<String> names = ["首页","会员","发布","通知","我的"];
-const List<String> images = ["res/drawable/ic_user_center_unselect.png","res/drawable/ic_user_center_unselect.png"
-                              ,"res/drawable/ic_user_center_unselect.png","res/drawable/ic_user_center_unselect.png"
-                              ,"res/drawable/ic_user_center_unselect.png"];
+const List<String> images = ["res/drawable/main_activity_bottom_study_normal.png","res/drawable/main_activity_bottom_study_normal.png"
+                              ,"res/drawable/main_activity_bottom_study_normal.png","res/drawable/main_activity_bottom_study_normal.png"
+                              ,"res/drawable/main_activity_bottom_study_normal.png"];
 const List<String> imageSelects = ["res/drawable/ic_user_center.png","res/drawable/ic_user_center.png"
                               ,"res/drawable/ic_user_center.png","res/drawable/ic_user_center.png"
                               ,"res/drawable/ic_user_center.png"];
@@ -13,9 +13,8 @@ const List<String> imageSelects = ["res/drawable/ic_user_center.png","res/drawab
 
 class MainTabLayout extends StatelessWidget {
 
-                            
-
-  final int _currentSelect = 0;
+  int _currentSelect = 0;
+  final List<MainTab> _mainTabList = List();
 
   @override
   Widget build(BuildContext context) {
@@ -26,27 +25,46 @@ class MainTabLayout extends StatelessWidget {
   }
 
   List<MainTab> _getMainTab(){
-    
-    List<MainTab> mainTabList = List();
     int i = 0;
     while(i < 5){
-      mainTabList.add(MainTab(i, _onPressed));
+      _mainTabList.add(MainTab(i, _onPressed));
       i++;
     }
-    return mainTabList;
+    return _mainTabList;
   }
 
   void _onPressed(int index){
       print(index);
+      if(index == _currentSelect){
+        
+      }else{
+        _currentSelect = index;
+        _mainTabList[index].select = false;
+      }
   }
 }
 
+// class MainTab extends StatefulWidget{
+
+//   MainTab({
+//     int index,
+//     Function onclick
+//   });
+
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     return _MainTabState(index,onclick);
+//   }
+// }
+
 class MainTab extends StatelessWidget{
 
-  final int _index = 0;
-  Function _onClick;
+  int _index = 0;
+  final Function _onClick;
+  bool select = false;
 
-  MainTab(index,this._onClick);
+  MainTab(_index,this._onClick);
 
   @override
   Widget build(BuildContext context) {
@@ -56,12 +74,13 @@ class MainTab extends StatelessWidget{
       child: InkWell(    // 一个 material 风格的响应控件
         onTap:(){
           _onClick(_index);
+          select = true;
         },
         child: Padding(
         padding: EdgeInsets.only(bottom: 6.0,top: 4.0),
         child: Column(
         children: <Widget>[
-          Image.asset(images[_index],width: 25,height: 25,),
+          Image.asset(_getImage(),width: 25,height: 25,),
           Text(
             names[_index],
             style: optionStyle,)
@@ -69,5 +88,15 @@ class MainTab extends StatelessWidget{
       ))
       )
       );
+  }
+  
+  String _getImage(){
+    print(images[_index]);
+    // print(imageSelects[_index]);
+    if(select){
+      return imageSelects[_index];
+    }else{
+      return images[_index];
+    }
   }
 }
