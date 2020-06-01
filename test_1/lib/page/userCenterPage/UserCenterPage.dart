c659269c00c440d6cb8f7a229bb82f3003231651
maintel <maintel@163.com>
2020-06-02 00:05:44 +0800

    跳转设置

--- test_1/lib/page/userCenterPage/UserCenterPage.dart
+++ test_1/lib/page/userCenterPage/UserCenterPage.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:test_1/page/settings/settingPage.dart';
 
 
 class UserCenterPage extends StatefulWidget{
@@ -11,12 +13,25 @@ class UserCenterPage extends StatefulWidget{
   }
 }
 
+void _goSetting(BuildContext context){
+  Navigator.push(context, 
+    MaterialPageRoute(builder: (context) => SettingPage()));
+}
+
 class UserCenterPageState extends State{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      child: Text("UserCenterPageState"),
+      child: Column(
+        children: <Widget>[
+          Text("user center page"),
+          RaisedButton(
+            onPressed: () => _goSetting(context),
+            child: Text("设置"),
+            )
+        ],
+      ),
     );
   }
 
